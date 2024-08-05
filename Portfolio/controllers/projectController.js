b3ea54d7c8197ab0a78cb15ab58cc74c0ec99715
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-06 01:33:28 +0300

    +project controller

---
+++ Portfolio/controllers/projectController.js
@@ -0,0 +1,75 @@
+const Project = require("../models/projectModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+
+exports.createProject = catchAsync(async (req, res, next) => {
+  const newProject = await Project.create(req.body);
+  if (!newProject) {
+    return next(new AppError("Project not created", 400));
+  }
+  res.status(201).json({
+    status: "success",
+    data: {
+      project: newProject,
+    },
+  });
+});
+
+exports.getProject = catchAsync(async (req, res, next) => {
+  const project = await Project.findById(req.params.id);
+  if (!project) {
+    return next(new AppError("No project found with that ID", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      project,
+    },
+  });
+});
+
+exports.getAllProjects = catchAsync(async (req, res, next) => {
+  const projects = await Project.find();
+
+  if (!projects) {
+    return next(new AppError("No projects found", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    results: projects.length,
+    data: {
+      projects,
+    },
+  });
+});
+
+exports.deleteProject = catchAsync(async (req, res, next) => {
+  const project = await Project.findByIdAndDelete(req.params.id);
+  if (!project) {
+    return next(new AppError("No project found with that ID", 404));
+  }
+
+  res.json({
+    status: "success",
+    data: null,
+  });
+});
+
+exports.updateProject = catchAsync(async (req, res, next) => {
+  const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!project) {
+    return next(new AppError("No project found with that ID", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      project,
+    },
+  });
+});
