f72a48b645177723c1792523d16d9e05d4cb2af6
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-05 18:07:48 +0300

    add populate middleware

--- Portfolio/models/userModel.js
+++ Portfolio/models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please provide a name"],
+    minlength: 3,
   },
   email: {
     type: String,
@@ -14,6 +15,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a password"],
   },
+  title: {
+    type: String,
+    required: [true, "Please provide a title"],
+    minlength: 10,
+    maxlength: 50,
+  },
   role: {
     type: String,
     enum: ["user", "admin"],
@@ -23,6 +30,34 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a description"],
   },
+  image: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+  },
+  projects: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+    },
+  ],
+  experiences: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Experience",
+    },
+  ],
+});
+
+// Populate user with projects and experiences
+userSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "projects",
+  }).populate({
+    path: "experiences",
+  }),
+    next();
 });
 
 const User = mongoose.model("User", userSchema);
