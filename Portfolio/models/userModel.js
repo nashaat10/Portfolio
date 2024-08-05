1ad4925cc1d8dba9f2b31bb07073cd5a2c82747e
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-05 16:24:41 +0300

    +up user model

--- Portfolio/models/userModel.js
+++ Portfolio/models/userModel.js
@@ -1,1 +1,29 @@
 const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide a description"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
