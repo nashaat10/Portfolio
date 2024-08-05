b1a1441fd0f8b265af558fafaab663df0c04aa02
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-05 16:08:14 +0300

    add server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`running in ${process.env.NODE_ENV} mode`);
+}
+
+app.use(express.json());
+
+module.exports = app;
