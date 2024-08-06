7361f712d41cdfc28d53e21f445cc44afc1315c5
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-06 09:55:02 +0300

    add routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const app = express();
 const userRouter = require("./Portfolio/routes/userRoutes");
 const projectRouter = require("./Portfolio/routes/projectRoutes");
+const experienceRouter = require("./Portfolio/routes/experienceRoutes");
 const AppError = require("./Portfolio/utils/appError");
 const globalErrorHandler = require("./Portfolio/controllers/errorController");
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/projects", projectRouter);
+app.use("/api/v1/experiences", experienceRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
