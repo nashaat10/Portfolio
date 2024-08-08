c3714907dcd1b27e4871466ad01960b0a623cc07
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-09 00:27:53 +0300

    + protect

--- Portfolio/routes/experienceRoutes.js
+++ Portfolio/routes/experienceRoutes.js
@@ -1,6 +1,9 @@
 const experienceController = require("../controllers/experienceController");
 const express = require("express");
+const authController = require("../controllers/authController");
+
 const router = express.Router();
+router.use(authController.protect);
 
 router
   .route("/")
