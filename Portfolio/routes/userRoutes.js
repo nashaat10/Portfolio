29758ca3c8f2b81cc88f71c259edcdf451d5f904
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-09 23:05:34 +0300

    +up routes

--- Portfolio/routes/userRoutes.js
+++ Portfolio/routes/userRoutes.js
@@ -8,10 +8,18 @@ router.post("/signUp", authController.signUp);
 router.post("/login", authController.login);
 
 router.use(authController.protect);
-router
-  .route("/")
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+
+router.route("/").get(userController.getAllUsers);
+// .post(userController.createUser);
+
+router.get("/me", userController.getMe, userController.getUser);
+
+router.patch(
+  "/updateMe",
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe
+);
 
 router
   .route("/:id")
