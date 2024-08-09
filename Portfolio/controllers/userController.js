6fed01831ecb12d1d0324535b588dd62c99d3a05
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-09 23:04:58 +0300

    up

--- Portfolio/controllers/userController.js
+++ Portfolio/controllers/userController.js
@@ -1,21 +1,57 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const multer = require("multer");
+const sharp = require("sharp");
 
-exports.createUser = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-  if (!newUser) {
-    return next(new AppError("User not created", 400));
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image! Please upload only images.", 400), false);
   }
-  res.status(201).json({
-    status: "success",
-    data: {
-      user: newUser,
-    },
-  });
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
 });
 
+exports.uploadUserPhoto = upload.single("photo");
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
+
+// exports.createUser = catchAsync(async (req, res, next) => {
+//   const newUser = await User.create(req.body);
+//   if (!newUser) {
+//     return next(new AppError("User not created", 400));
+//   }
+//   res.status(201).json({
+//     status: "success",
+//     data: {
+//       user: newUser,
+//     },
+//   });
+// });
+
 exports.getUser = catchAsync(async (req, res, next) => {
+  // console.log(req.params.id);
+  // console.log(req.user);
+
   const user = await User.findById(req.params.id)
     .populate("projects")
     .populate("experiences");
@@ -74,5 +110,52 @@ exports.updateUser = catchAsync(async (req, res, next) => {
     data: {
       user,
     },
+  });
+});
+
+exports.getMe = catchAsync(async (req, res, next) => {
+  req.params.id = req.user._id;
+  console.log(req.params.id);
+  console.log(req.user._id);
+  next();
+});
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.confirmPassword) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword.",
+        400
+      )
+    );
+  }
+  const filterBody = filterObj(req.body, "name", "email");
+  if (req.file) filterBody.photo = req.file.filename;
+
+  console.log(req.user._id);
+  const updateUser = await User.findByIdAndUpdate(req.user._id, filterBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updateUser,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });
