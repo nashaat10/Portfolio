c71563e3e8299908fed7796e8eafee31503c0fe5
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-30 08:23:34 +0300

    up port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config({ path: "config.env" });
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
